b5194061aa7fadbab10962419c7ac56acd951c52
vijay karthikeyan <vijaykarthik257@gmail.com>
2023-02-14 16:42:56 +0530

    2nd commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,10 @@ import FooterComponent from "../components/footer";
 
 const HomePage = () => {
     var response;
-    const data = async () =>{
+    const datvaluea = async () =>{
          response =  await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-01-14&sortBy=publishedAt&apiKey=e9cb0b1dd6aa46feb86017004dc6fdfe',);
       }
-      data();
+      data();   
     return(
         <div className="main-page">
             <HeaderComponent></HeaderComponent>
